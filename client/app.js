529526a1290b0929bf76b4f8790282ef3c5366ed
Leslie D <37258420+leslied2018@users.noreply.github.com>
2018-09-20 18:30:47 -0700

    Add files via upload

---
+++ client/app.js
@@ -0,0 +1,7 @@
+$('form').on('submit',function () {
+  var text = $('#message').val();
+  alert(text);
+  return false;
+});
+
+/* event-driven programming: asking to be notified when a given event takes place and waiting for it to happen*/
